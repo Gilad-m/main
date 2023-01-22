0cb9102c6d2b8131b9ef5704eb1caff5bffabc5c
Gilad-m <120260983+Gilad-m@users.noreply.github.com>
2023-01-22 22:19:26 +0200

    Create Read.pl

---
+++ Read.pl
@@ -0,0 +1,6 @@
+Game in batch ile
+
+The program chooses a random number between 0-100.
+It will tell you if your guess is too high, too low, or correct!
+
+This game was written in the batch programming language on a WINDOWS 7 version of notepad.
